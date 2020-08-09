c40f1209b6b751dd66719aa0f8662539b94af2d2
Michael Grandori <41161951+manthonyg@users.noreply.github.com>
2020-08-08 21:19:34 -0700

    heroku commit

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ db.create_all()
 db.Model.metadata.reflect(db.engine)
 
 
-class Users(db.Model):
+class User(db.Model):
     __tablename__ = 'users'
     __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
@@ -24,7 +24,7 @@ class Users(db.Model):
         return 'username:{}-password:{}'.format(self.username, self.password)
 
 
-class Dogs(db.Model):
+class Dog(db.Model):
     __tablename__ = 'dogs'
     __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
@@ -34,40 +34,24 @@ class Dogs(db.Model):
     shape = db.Column(db.Integer)
     activity = db.Column(db.Integer)
     weight = db.Column(db.Integer)
+    KGS = db.Column(db.Integer)
+    RER = db.Column(db.Integer)
+    MER = db.Column(db.Integer)
+    breedStats = db.Column(db.String(100000))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    def __init__(self, name, age, breed, shape, activity, weight, user_id):
+    def __init__(self, name, age, breed, shape, activity, weight, KGS, RER, MER, breedStats, user_id):
         self.name = name
         self.age = age
         self.breed = breed
         self.shape = shape
         self.activity = activity
         self.weight = weight
-        self.user_id = user_id
-
-    def __repr__(self):
-        return 'name:{}-age:{}-breed:{}-shape:{}-activity:{}-weight:{}-user_id:{}'.format(self.name, self.age, self.breed, self.shape, self.activity, self.weight, self.user_id)
-
-
-class Results(db.Model):
-    __tablename__ = 'results'
-    __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    dog_id = db.Column(db.Integer, db.ForeignKey('dogs.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    KGS = db.Column(db.Integer)
-    RER = db.Column(db.Integer)
-    MER = db.Column(db.Integer)
-    suggested_calories = db.Column(db.Integer)
-    dog = db.relationship(Dogs, backref='dog')
-
-    def __init__(self, dog_id, user_id, KGS, RER, MER, suggested_calories):
-        self.dog_id = dog_id
-        self.user_id = user_id
         self.KGS = KGS
         self.RER = RER
         self.MER = MER
-        self.suggested_calories = suggested_calories
+        self.breedStats = breedStats
+        self.user_id = user_id
 
     def __repr__(self):
-        return 'suggested_calories{}-KGS:{}-RER:{}-MER:{}-DOG_ID{}'.format(self.suggested_calories, self.KGS, self.RER, self.MER, self.dog_id)
+        return 'name:{}-age:{}-breed:{}-shape:{}-activity:{}-weight:{}-KGS:{}-RER:{}-MER:{}-user_id:{}'.format(self.name, self.age, self.breed, self.shape, self.activity, self.weight, self.KGS, self.RER, self.MER, self.user_id)
--- helpers.py
+++
@@ -1,5 +0,0 @@
-def calculateReqCalories(lbs, multiplier):
-    kgs = int(round(int(lbs) / int(2.2), 2))
-    RER = int(round(int(70) * int((kgs**0.75)), 2))
-    MER = round(int(RER) * float(multiplier))
-    return MER
