475860990547a99f53317b68feed645d1abbf99d
Michael Grandori <41161951+manthonyg@users.noreply.github.com>
2020-08-08 22:54:12 -0700

    autoincrement

--- db.py
+++ db.py
@@ -15,7 +15,7 @@ db.Model.metadata.reflect(db.engine)
 class User(db.Model):
     __tablename__ = 'users'
     __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
 
