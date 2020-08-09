675f70e8963cea2450c65f778d7e6fb8649188f2
Michael Grandori <41161951+manthonyg@users.noreply.github.com>
2020-08-08 17:15:35 -0700

    simplified db structure, styling changes to dashboard

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, session, render_template, request, redirect, url_for, flash
+from flask import Flask, jsonify, session, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 from functools import wraps
-from helpers import calculateReqCalories
-from db import db, app, Users, Dogs, Results
+from db import db, app, User, Dog
 import requests
 import json
 
@@ -18,41 +17,55 @@ def after_request(response):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    previousResults = db.session.query(Dog).order_by(Dog.id).all()
     if request.method == 'POST':
+
         name = request.form.get('dog-name')
         age = request.form.get('dog-age')
         breed = request.form.get('dog-breed')
         shape = request.form.get('dog-shape')
         activity = request.form.get('dog-activity')
         weight = request.form.get('dog-weight')
-        user_id = session['id']
-        newDog = Dogs(name=name, age=age, breed=breed,
-                      shape=shape, activity=activity, weight=weight, user_id=user_id)
-        db.session.add(newDog)
-
         KGS = int(round(int(weight) / int(2.2), 2))
         RER = int(round(int(70) * int((KGS**0.75)), 2))
         MER = round(int(RER) * float(activity))
-
-        dog = Dogs.query.filter_by(
-            name=name, age=age, breed=breed, activity=activity).first()
-
-        newResult = Results(suggested_calories=calculateReqCalories(weight, activity),
-                            KGS=KGS, RER=RER, MER=MER, user_id=user_id, dog_id=dog.id)
-
-        db.session.add(newResult)
-        db.session.commit()
-
-        previousResults = db.session.query(Dogs, Results).outerjoin(
-            Results, Dogs.id == Results.dog_id).filter(Results.user_id == session['id']).limit(5).all()
-
         breedStats = requests.get(
-            f'https://api.thedogapi.com/v1/images/search?breed_ids={dog.breed}')
+            f'https://api.thedogapi.com/v1/images/search?breed_ids={breed}')
+        print(breedStats.text)
+        user_id = session['id']
+        newDog = Dog(name=name, age=age, breed=breed,
+                     shape=shape, activity=activity, weight=weight, KGS=KGS, RER=RER, MER=MER, breedStats=breedStats.text, user_id=user_id)
 
-        return render_template('results.html', suggestedCalories=calculateReqCalories(weight, activity), name=name, weight=weight, KGS=KGS, RER=RER, MER=MER, activity=activity, breedStats=json.loads(breedStats.text), username=session['username'], previousResults=previousResults)
+        db.session.add(newDog)
+        db.session.commit()
+        print(newDog.breedStats)
+
+        return render_template('dashboard.html', new_assessment=False, result=True, name=name, weight=weight, KGS=KGS, RER=RER, MER=MER, activity=activity, breedStats=json.loads(newDog.breedStats), username=session['username'], previousResults=previousResults)
+
     if 'username' in session:
-        return render_template('dashboard.html', username=session['username'])
+        return render_template('dashboard.html', username=session['username'], assessment=True, previousResults=previousResults)
+
     return render_template('login.html')
+
+
+@ app.route('/delete_result')
+def deleteResult():
+    dogId = request.args.get('result-id')
+    dog = Dog.query.filter_by(id=dogId).first()
+    db.session.delete(dog)
+    db.session.commit()
+    flash(f"Dog #{dog.name} was successfully deleted")
+    return redirect(url_for('index'))
+
+
+@ app.route('/view_result')
+def viewResult():
+    previousResults = db.session.query(Dog).order_by(Dog.id).all()
+    dogId = request.args.get('dogId')
+    dog = Dog.query.filter_by(id=dogId).first()
+
+    print(type(jsonify(dog.breedStats)))
+    return render_template('dashboard.html', new_assessment=False, result=True, name=dog.name, weight=dog.weight, KGS=dog.KGS, RER=dog.RER, MER=dog.MER, activity=dog.activity, breedStats=json.loads(dog.breedStats), username=session['username'], previousResults=previousResults)
 
 
 @ app.route('/login', methods=['GET', 'POST'])
@@ -64,20 +77,16 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
         # Ensure username was submitted
-        if not username:
-            return flash("must provide username")
-        # Ensure password was submitted
-        if not password:
-            return flash("must provide password")
 
         incomingUser = Users.query.filter_by(username=username).first()
-
-        if not incomingUser.username or not check_password_hash(incomingUser.password, password):
+        if not incomingUser or not check_password_hash(incomingUser.password, password):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            print('invalid useername or password')
+            return redirect(url_for('index'))
 
         session['username'] = request.form['username']
         session['id'] = incomingUser.id
+
         return redirect(url_for('index'))
 
     return render_template('login.html')
